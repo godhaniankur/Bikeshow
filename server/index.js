dee36f554ae268526fc53f04d6497ef3f15aad25
Ankur Godhani <ankurgodhani128@gmail.com>
2024-12-06 13:49:16 +0530

    Update files

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ const app = express();
  app.use(cookieParser())
 
  app.use(cors({
-       origin:"http://localhost:3000",
+       origin:"https://bikeshow-dun.vercel.app",
        credentials:true
  }))
 
